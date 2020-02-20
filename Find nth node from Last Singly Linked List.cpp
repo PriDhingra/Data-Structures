2988e7793b6edd9cefbd1cb7b0fcba5351faace5
PriDhingra <princedhingra25@gmail.com>
2020-02-20 23:54:13 +0530

    Add files via upload

---
+++ Find nth node from Last Singly Linked List.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+struct Node* tail = NULL;
+int count = 0;
+
+void insertAtBeginning(int new_data){
+    struct Node* new_node= new Node();
+    count++;
+    new_node->data = new_data;
+    new_node->next = head;
+    head = new_node;
+}
+
+void insertAtEnd(int new_data){
+    struct Node* new_node = new Node();
+    count++;
+    struct Node* ftr;
+    ftr = head;
+    new_node->data = new_data;
+    new_node->next = NULL;
+    while(ftr->next != NULL){
+        ftr = ftr->next;
+    }
+    ftr->next = new_node;
+}
+
+void insertAtPosition(int pos,int new_data){
+    if(pos == 1){
+        insertAtBeginning(new_data);
+    }
+    struct Node* new_node = new Node();
+    count++;
+    new_node->data = new_data;
+    struct Node* ftr;
+    ftr = head;
+    while((pos-1) > 0){
+        ftr = ftr->next;
+        pos--;
+    }
+    new_node->next = ftr->next;
+    ftr->next = new_node;
+}
+
+void deleteAtBeginning(){
+    struct Node* ftr;
+    ftr = head;
+    head = head->next;
+    free(ftr);
+    count--;
+}
+
+void deleteAtEnd(){
+    struct Node* ftr1;
+    struct Node* ftr2;
+    ftr1 = head;
+    ftr2 = head;
+    int count1 = 0;
+    while(ftr1->next != NULL){
+        ftr1 = ftr1->next;
+        if(count1 != 0){
+            ftr2 = ftr2->next;
+        }
+        count1++;
+        
+    }
+    ftr2->next = NULL;
+    free(ftr1->next);
+    count--;
+}
+
+void deleteAtPosition(int pos){
+    if(pos == 1){
+        deleteAtBeginning();
+    }
+    struct Node* ftr1;
+    struct Node* ftr2;
+    ftr1 = head;
+    ftr2 = head;
+    int count1 = 0;
+    while((pos-1) > 0){
+        ftr1 = ftr1->next;
+        if(count1 != 0){
+            ftr2 = ftr2->next;
+        }
+        pos--;
+        count1++;
+    }
+    ftr2->next = ftr1->next;
+    free(ftr1);
+    count--;
+}
+
+void searchNodeFromLast(int pos){
+     if(pos > count){
+         cout<<"\n\n\nPlease enter the position no. which is less than "<<count<<"!!"; 
+     }else{
+          int pos1 = pos;
+          struct Node* ftr1 = head;
+          struct Node* ftr2 = head;
+          while(ftr1->next != NULL){
+               ftr1 = ftr1->next;
+               if(pos <= 1){
+                    ftr2 = ftr2->next;
+               }
+               pos--;
+          }
+          cout<<"\n\n\nData at Node No. "<<pos1<<" from last is: "<<ftr2->data;
+     }
+}
+
+void display(){
+    struct Node* ftr;
+    ftr = head;
+    while(ftr != NULL){
+        cout<<ftr->data<<"  "<<ftr->next;
+        cout<<"\n";
+        ftr = ftr->next;
+    }
+    cout<<"\n\nLength of Linked List is: "<<count;
+}
+
+int main()
+{
+   insertAtBeginning(1);
+   insertAtBeginning(2);
+   insertAtEnd(9);
+   insertAtEnd(10);
+   insertAtBeginning(3);
+   insertAtBeginning(4);
+   insertAtEnd(5);
+   insertAtEnd(6);
+   insertAtEnd(7);
+   insertAtEnd(8);
+   insertAtPosition(3,11);
+   insertAtPosition(5,12);
+   insertAtPosition(7,13);
+   insertAtPosition(8,14);
+   deleteAtBeginning();
+   deleteAtBeginning();
+   deleteAtEnd();
+   deleteAtEnd();
+   deleteAtEnd();
+   deleteAtPosition(4);
+   
+   display(); 
+   
+   searchNodeFromLast(1);
+   searchNodeFromLast(4);
+   searchNodeFromLast(6);
+   searchNodeFromLast(7);
+   searchNodeFromLast(9);
+   searchNodeFromLast(13);
+   
+   return 0;
+}
